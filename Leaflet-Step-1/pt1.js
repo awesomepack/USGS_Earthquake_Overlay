356339099c7be3d1ba68dbb9d6da665fb2d62345
Merari Cisneros <meraridanielcisneros@gmail.com>
2021-09-02 22:00:53 -0700

    Added map object and street map tile layer

--- Leaflet-Step-1/pt1.js
+++ Leaflet-Step-1/pt1.js
@@ -10,10 +10,42 @@ d3.json(endpoint).then(function(data) {
     console.log(data.features[0].properties)
 
     // The structure of the coordinate data and depth
-    console.log(data.features[0].geometry.coordinates)
+    console.log(data.features[0].geometry.coordinates.slice(0,2))
+
+    // calling the creatMap() function
+    createMap(data);
 });
 
 // create the map function we call on the data
 
+function createMap(EarthquakeData){
+
+    // Generating the map object
+    var myMap = L.map('map' , {
+        center: [35.75,-39.46] ,
+        zoom: 3
+    });
+
+    // Adding the open street map tile layer
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(myMap);
+
+    // Create a marker for each Earthquake object
+    // Object in question is in: EarthquakeData.features
+    // magnitude of an earthquake will be radius
+    // depth of earthquake is color
+
+    var magnitude = EarthquakeData.features[0].properties.mag
+    
+    var depth = EarthquakeData.features[0].geometry.coordinates[2]
+
+    console.log(depth);
+
+
+
+
+
+};
 
 
